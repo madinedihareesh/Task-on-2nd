99e1d7454d6f6dfa46bb5e9ce1a6c6e55e1b48a8
madinedihareesh <hareesh.madinedi@gmail.com>
2015-11-02 18:05:23 +0530

    update

---
+++ File Browser/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace File_Browser
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        FolderPicker fp = new FolderPicker();
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            fp.SuggestedStartLocation = PickerLocationId.Desktop;
+            fp.ViewMode = PickerViewMode.Thumbnail;
+            fp.FileTypeFilter.Add("*");
+            StorageFolder sf = await fp.PickSingleFolderAsync();
+
+            if (sf != null)
+            {
+
+                IReadOnlyList<IStorageItem> read = await sf.GetItemsAsync();
+                g.ItemsSource = read;
+                g.DisplayMemberPath = "name";
+               // g.CompleteViewChange();
+               
+                
+            }
+        }
+
+        
+
+       
+    }
+}
